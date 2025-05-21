83142b1eff060886435dc176ce647b83fdaa95ff
DanishBukhari <dani.syed.18@gmail.com>
2025-05-21 11:36:24 -0400

    sdfs

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/activate-addon', (req, res) => {
     response_type: 'code',
     client_id: CLIENT_ID,
     redirect_uri: REDIRECT_URI,
-    scope: 'company staff job customer',
+    scope: 'write_companies read_job read_companies',
   });
   return res.redirect(authorizationUrl);
 });
